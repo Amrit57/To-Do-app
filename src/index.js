d2d3a0adb45b6737e3d52fdbab1615776907b59a
Amrit57 <fenpadorje7@gmail.com>
2023-05-28 10:37:43 +0545

    Linked js modules fies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import hello from './home.js'
 
 function component() {
-   
-    return element;
-  }
-  
-  // document.body.appendChild(component());
+  const element = hello();
+  console.log('hehehe')
+  return element;
+}
+
+document.body.appendChild(component());
