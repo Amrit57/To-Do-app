4ecdc853dfcd7f0ffd568d55cf21c8ecc2440e40
Amrit57 <fenpadorje7@gmail.com>
2023-05-29 20:41:27 +0545

    Styled todo-items and added styles

---
+++ src/home.js
@@ -0,0 +1,8 @@
+import { addTodoitems, formEventHandler, todoList } from './todo.js'
+
+export default function home() {
+    addTodoitems();
+    console.log(hello)
+    console.log(todoList);
+}
+home()
